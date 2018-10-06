2376ffa40b0fb4e1faa06b36989b82c44af25891
Ryan Stevens <stevens.ryan@gmail.com>
2018-10-05 17:27:53 -0700

    fixing build path

--- bin/build-node.js
+++ bin/build-node.js
@@ -8,7 +8,7 @@ async function build() {
 
   fs_extra.emptyDirSync(paths.app_dist);
 
-  child_process.spawn(path.normalize(__dirname+'/../node_modules/.bin/babel'), ['src', '-d', paths.app_dist, '--source-maps', '--copy-files'],
+  child_process.spawn(path.normalize(__dirname+'/../../.bin/babel'), ['src', '-d', paths.app_dist, '--source-maps', '--copy-files'],
     {
       stdio: [process.stdin, process.stdout, process.stderr]
     }
