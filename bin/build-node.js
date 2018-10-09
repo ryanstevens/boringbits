7f98b6331c8e4af750b793f28d05c63a01d70ec7
Ryan Stevens <stevens.ryan@gmail.com>
2018-10-08 23:50:40 -0700

    fixing path issues for symlinked instances

--- bin/build-node.js
+++ bin/build-node.js
@@ -3,12 +3,21 @@ const fs_extra = require('fs-extra');
 const path = require('path')
 const paths = require('../dist/node_modules/paths')
 const child_process = require('child_process')
+const fs = require('fs');
 
 async function build() {
 
   fs_extra.emptyDirSync(paths.app_dist);
+  let babelPath = '/../.bin/babel';
+  try {
+    fs.statSync(path.normalize(__dirname+babelPath));
+  } catch(e) {    
+    babelPath =  '/../node_modules/.bin/babel';
+    fs.statSync(path.normalize(__dirname+babelPath));
+  }
+  
 
-  child_process.spawn(path.normalize(__dirname+'/../../.bin/babel'), ['src', '-d', paths.app_dist, '--source-maps', '--copy-files'],
+  child_process.spawn(path.normalize(__dirname+babelPath), ['src', '-d', paths.app_dist, '--source-maps', '--copy-files'],
     {
       stdio: [process.stdin, process.stdout, process.stderr]
     }
