c8bbbb4c990dfd19fccda9adec2ca2d4c6d45391
Ryan Stevens <stevens.ryan@gmail.com>
2018-10-23 10:21:52 -0700

    adding memory to health check

--- src/lib/init-hooks/core-hooks/healthy.js
+++ src/lib/init-hooks/core-hooks/healthy.js
@@ -1,20 +1,45 @@
-const HealthCheck = require('healthy').HealthCheck;
+const healthy = require('healthy');
+const os = require('os');
 
-module.exports = function healthy(BoringInjections) {
+module.exports = function(BoringInjections) {
   
   const {
     boring
   } = BoringInjections;
 
-  const healthy = new HealthCheck({}, {
-    started: new Date()
+  const {HealthCheck, HealthModel} = healthy;
+
+  const healthCheck = new HealthCheck({}, {
+    started: new Date(),
+    hostname: os.hostname()
   });
+
+  function getHeap() {
+    const mem = process.memoryUsage();
+    return Object.keys(mem).reduce((acc, key)  => {
+      acc[key] = Math.ceil(mem[key] / 1024 / 1024 * 100) / 100;
+      return acc;
+    }, {});
+  }
+
+  const memoryHealth = new HealthModel(getHeap(), {
+    checkInterval: 15000,
+    asyncCheck: function(cb) {
+      const heap = getHeap();
+      Object.keys(heap).forEach(key => {
+        memoryHealth.set(key, heap[key]);
+      })
+      cb();
+    }
+  });
+
+  healthCheck.addChildCheck('memory', memoryHealth);
 
   boring.before('add-hooks', function() {
 
     boring.before('app.use', function(ctx) {
       if (ctx.name === 'boring-session') {
-        boring.app.use(healthy.createMiddleware());
+        boring.app.use(healthCheck.createMiddleware());
       }
       return Promise.resolve();
     });
