4a16ed466cb4d3fb508bc6787a8c9e99829df5b9
Ryan Stevens <stevens.ryan@gmail.com>
2018-11-10 15:45:36 -0600

    adding default subdomain support

---
+++ src/lib/init-hooks/core-hooks/proxy.js
@@ -0,0 +1,15 @@
+module.exports = function(BoringInjections) {
+
+  const {
+    boring
+  } = BoringInjections;
+
+  boring.before('add-hooks', function() {
+
+    boring.app.set('trust proxy', true);
+    return Promise.resolve();
+
+  });
+
+  return {name: 'trust-proxy'}
+}
