2bb0acf0525efda9cb81919873b5be427cdb19c0
Ryan Stevens <stevens.ryan@gmail.com>
2018-11-12 12:40:42 -0800

    ensure unit tests always point to /src

--- config/testing.js
+++ config/testing.js
@@ -1,12 +1,17 @@
-// This file exists because 
+// This file exists because
 // we want to limit side effects
-// of running config values set 
-// either in development or production 
+// of running config values set
+// either in development or production
 
 module.exports = {
   use_webpack_dev_server: false,
   foo: {
     bar: 'overriden',
     baz: 'meep'
+  },
+  boring: {
+    paths: {
+      base_app_path: 'src'
+    }
   }
 }
