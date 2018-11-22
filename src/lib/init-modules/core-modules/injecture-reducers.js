39cca91299114624c73df1d1bbe33bc9406d7830
Ryan Stevens <stevens.ryan@gmail.com>
2018-11-21 23:05:43 -0800

    adding ability to create reducers within jecture

---
+++ src/lib/init-modules/core-modules/injecture-reducers.js
@@ -0,0 +1,24 @@
+
+module.exports = function(BoringInjections) {
+
+  const {
+    injecture,
+    config,
+  } = BoringInjections;
+
+  const boringStrategies = config.get('boring.strategies', {});
+  const projStrategies = config.get('strategies', {});
+
+  function createStrategyReducer(key, strategy) {
+    injecture.addReducer(key, function(keys) {
+      return keys.filter(keyObj => (keyObj.key === strategy));
+    });
+  }
+
+  const combinedStrategies = Object.assign({}, boringStrategies, projStrategies);
+
+  Object.keys(combinedStrategies).forEach(key => {
+    const strategy = combinedStrategies[key];
+    createStrategyReducer(key, strategy);
+  });
+};
---
+++ src/lib/init-modules/test/injecture-reducer-test.js
@@ -0,0 +1,77 @@
+const assert = require('assert');
+const injectureDeps = require('../core-modules/injecture-reducers');
+
+describe.only('Injecture strategy reducers', function() {
+
+  it('will reduce an interface to one key by config', async () => {
+
+      const injecture = require('injecture');
+      const config = {
+        get: function(key) {
+          if (key === 'boring.strategies') return {
+            'animal' : 'ClassDog',
+            'car': 'ClassFord'
+          };
+          if (key === 'strategies') return {
+            'car': 'ClassChevy',
+            'computer': 'ClassMac'
+          }
+        }
+      }
+      injectureDeps({
+        injecture,
+        config
+      });
+
+      injecture.registerClass( class ClassChevy {
+        constructor() {
+        }
+      }, {
+        interfaces: ['car'],
+        singleton: true,
+      });
+
+      injecture.registerClass( class ClassFord {
+        constructor() {
+        }
+      }, {
+        interfaces: ['car'],
+        singleton: true,
+      });
+
+      injecture.registerClass( class ClassGM {
+        constructor() {
+        }
+      }, {
+        interfaces: ['car'],
+        singleton: true,
+      });
+
+      injecture.registerClass( class ClassDog {
+        constructor() {
+        }
+      }, {
+        interfaces: ['animal'],
+        singleton: true,
+      });
+
+
+      injecture.registerClass( class ClassCat {
+        constructor() {
+        }
+      }, {
+        interfaces: ['animal'],
+        singleton: true,
+      });
+
+      const carKey = injecture.getKeysByInterface('car');
+      assert.equal(carKey.length, 1);
+      assert.equal(carKey[0], 'ClassChevy');
+
+
+      const animalKey = injecture.getKeysByInterface('animal');
+      assert.equal(animalKey.length, 1);
+      assert.equal(animalKey[0], 'ClassDog');
+
+  });
+});
