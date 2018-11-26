7ac783050d3d403b9b5a582038059bd9ef70fef5
Ryan Stevens <stevens.ryan@gmail.com>
2018-11-25 16:43:10 -0800

    expose more transitive modules to client

--- client.js
+++ client.js
@@ -1,4 +1,17 @@
+/**
+ * This module will NOT be babel'd into dist
+ * as it is meant to be required by client side
+ * code via `require('boringbits/client')`
+ * So please write vanilla old school JS
+ */
+
+var react = require('react');
+var redux = require('redux');
+var injecture = require('./dist/node_modules/injecture');
 
 module.exports = {
   renderRedux: window.__boring.renderRedux,
+  react: react,
+  redux: redux,
+  injecture: injecture
 };
