7eadf1a4639a3e35b86f59de1f55c9d45707362a
Ryan Stevens <stevens.ryan@gmail.com>
2018-12-18 18:45:01 -0800

    removing /dist files

--- dist/lib/decorators/react.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-const toExport = {};
-module.exports = toExport;
-//# sourceMappingURL=react.js.map
--- dist/lib/init-hooks/core-hooks/react/dynamicComponents/dist/keep.js
+++
@@ -1,3 +0,0 @@
-// noop, this file simply ensure's there is a directory copied into /dist/lib/init-hooks/core-hooks/react/dynamicComponents/dist
-"use strict";
-//# sourceMappingURL=keep.js.map
