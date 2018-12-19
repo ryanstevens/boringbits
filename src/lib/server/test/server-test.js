6b9c8968a0cf3f018d3281eb1cbbaa1d23e4e9c3
Ryan Stevens <stevens.ryan@gmail.com>
2018-12-18 19:02:09 -0800

    fixing unit tests

--- src/lib/server/test/server-test.js
+++ src/lib/server/test/server-test.js
@@ -4,41 +4,39 @@ const logger = require('boring-logger');
 
 describe('Boring Server', function() {
 
+  // eslint-disable-next-line no-invalid-this
   this.timeout(20000);
   let Server;
   beforeEach(() => {
     const init = proxyquire('../../init-pipeline', {
       express: function appToReturn() {
-          return {
-            listen: ((port, fn) => fn()),
-            use: function() {},
-            get: function() {},
-            post: function() {},
-            set: function() {}
-          }
-        }
-      }
-    )
+        return {
+          listen: ((port, fn) => fn()),
+          use: () => {},
+          get: () => {},
+          post: () => {},
+          set: () => {},
+          disable: () => {},
+        };
+      },
+    });
 
-    Server = proxyquire('../index', {
-      '../init-pipeline': init
-      }
-    )
-  })
+    Server = proxyquire('../index', {'../init-pipeline': init});
+  });
 
   it('start can take an options callback', async function() {
 
     const server = new Server();
     server.before('listen', async function(bootOptions) {
       assert.ok(bootOptions.webpack_config, 'should have access to webpack in before hook');
-      bootOptions.mutateMe = 'ryan'
+      bootOptions.mutateMe = 'ryan';
     });
 
-    const final_config = await server.start();
+    const finalConfig = await server.start();
 
-    assert.ok(final_config.boring.app, 'should have access to express');
-    assert.ok(final_config.mutateMe, 'ryan', 'before hook did not run');
-    assert.ok(final_config.webpack_config, 'There should be a webpack object');
+    assert.ok(finalConfig.boring.app, 'should have access to express');
+    assert.ok(finalConfig.mutateMe, 'ryan', 'before hook did not run');
+    assert.ok(finalConfig.webpack_config, 'There should be a webpack object');
 
   });
 
