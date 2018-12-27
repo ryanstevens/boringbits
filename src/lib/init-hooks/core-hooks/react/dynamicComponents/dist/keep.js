e1c3be145671a6e8203225320acf9edff27251b9
Ryan Stevens <stevens.ryan@gmail.com>
2018-12-26 16:00:36 -0800

    adding npm run clean to remove dist files

--- src/lib/init-hooks/core-hooks/react/dynamicComponents/dist/keep.js
+++ src/lib/init-hooks/core-hooks/react/dynamicComponents/dist/keep.js
@@ -1,1 +1,2 @@
 // noop, this file simply ensure's there is a directory copied into /dist/lib/init-hooks/core-hooks/react/dynamicComponents/dist
+// ALSO, this has to be a .js file so this directory is copied / created by babel
