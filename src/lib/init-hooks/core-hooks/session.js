e415615f61969f56e11bf67174bc59adc13aad80
Ryan Stevens <stevens.ryan@gmail.com>
2018-12-27 13:13:55 -0800

    add proper http logging WITH correlationID support

--- src/lib/init-hooks/core-hooks/session.js
+++ src/lib/init-hooks/core-hooks/session.js
@@ -1,9 +1,9 @@
-var cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 
 module.exports = function(BoringInjections) {
-  
+
   const {
-    boring
+    boring,
   } = BoringInjections;
 
   boring.after('add-hooks', function() {
@@ -13,14 +13,12 @@ module.exports = function(BoringInjections) {
       keys: [boring.config.get('boring.session.key1', 'woiejf*EFWjsd78yg@'), boring.config.get('boring.session.key2', '54we54fFEIH8y4tlkajE*Ygt@')],
 
       // Cookie Options
-      maxAge:  boring.config.get('boring.session.duration', 24) * 60 * 60 * 1000 // 24 hours
-    }))
+      maxAge: boring.config.get('boring.session.duration', 24) * 60 * 60 * 1000, // 24 hours
+    }));
 
     return Promise.resolve();
 
   });
 
-  
-
-  return {name: 'boring-session'}
-}
+  return {name: 'boring-session'};
+};
--- src/lib/init-middleware/core-middleware/parsers.js
+++ src/lib/init-middleware/core-middleware/parsers.js
@@ -1,6 +1,4 @@
-const serveStatic = require('serve-static')
-const path = require('path');
-const compose = require('compose-middleware').compose
+const compose = require('compose-middleware').compose;
 
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -9,12 +7,12 @@ const bodyParser = require('body-parser');
 module.exports = function boringInection(boring) {
 
   boring.before('init-routers', function() {
-    boring.app.use(boring.middleware.parsers)
-  })
+    boring.app.use(boring.middleware.parsers);
+  });
 
   return compose([
     bodyParser.json(),
-    bodyParser.urlencoded({ extended: false }),
-    cookieParser()
-  ])
+    bodyParser.urlencoded({extended: false}),
+    cookieParser(),
+  ]);
 };
