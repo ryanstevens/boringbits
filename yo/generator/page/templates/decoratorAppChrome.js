b652fcc8d3059f0657ffb6dfc8dd904b8b5ee354
Ryan Stevens <stevens.ryan@gmail.com>
2018-12-28 01:47:06 -0800

    adding app chrome example in generator

---
+++ yo/generator/page/templates/decoratorAppChrome.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {withStyles} from '@material-ui/core/styles';
+import {makeDecorator} from 'boringbits/react';
+
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+
+
+@withStyles(theme => ({
+  outerContainer: {
+    flexGrow: 1,
+    height: '100%',
+    backgroundColor: '#eee',
+    overflowY: 'auto',
+  },
+  container: {
+    width: '100%',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+}))
+class AppChrome extends React.Component {
+
+  render() {
+    const {classes} = this.props;
+    return (
+      <Grid container className={classes.outerContainer} justify="center" spacing={0}>
+        <Grid className={classes.container} item>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit" className={classes.grow}>
+                My App
+              </Typography>
+            </Toolbar>
+          </AppBar>
+
+          {this.props.children}
+        </Grid>
+      </Grid>
+
+    );
+  }
+}
+
+export default makeDecorator(AppChrome);
