1ba78a8a7d55114046a36f30e5c52160c41ac92a
Ryan Stevens <stevens.ryan@gmail.com>
2019-02-20 22:58:21 -0800

    ensring rootPath is set on register

--- register.js
+++ register.js
@@ -2,15 +2,18 @@
  * Will babelify a code base using
  * boring's presets.
  *
- * This is intended primarily for dev tasks 
- * such as unit tests.  You should 
- * generally not need to use this 
- * in your main application during 
- * 
- * Usage Examples: 
- * 
+ * This is intended primarily for dev tasks
+ * such as unit tests.  You should
+ * generally not need to use this
+ * in your main application during
+ *
+ * Usage Examples:
+ *
  * - if (dev) require('boringbits/register');
  * - node node_modules/mocha/bin/_mocha  --require boringbits/register src/tests/my-test.js
  */
 
- require('./dist/babel_register');
+const nodeAppPath = require('./dist/node_modules/app-module-path');
+nodeAppPath.addPath(process.cwd() + '/src');
+
+require('./dist/babel_register');
